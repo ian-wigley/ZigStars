7f76cf127a4d9b2e51356c32e61dd458634efba0
Ian Wigley <ian.wigley@outlook.com>
2026-07-10 06:07:25 +0100

    Update stars.zig

--- src/stars.zig
+++ src/stars.zig
@@ -27,22 +27,18 @@ pub const Star = struct {
     pub fn update(self: *Star) void {
         self.speed = self.speed - 0.0175;
         self.angle = self.angle + 0.025;
-        // print("Angle:  {any}", .{self.angle});
         const tempX: f64 = @sin(self.angle) * self.speed * 10;
         const tempY: f64 = @cos(self.angle) * self.speed * 10;
-        const painX: i32 = @intFromFloat(tempX);
-        const painY: i32 = @intFromFloat(tempY);
-        self.x = painX + self.x;
-        self.y = painY + self.y;
+        const newX: i32 = @intFromFloat(tempX);
+        const newY: i32 = @intFromFloat(tempY);
+        self.x = newX + self.x;
+        self.y = newY + self.y;
         if (self.x < 0 or self.x > self.width or self.y < 0 or self.y > self.height) {
-            // print("Angle before update: {any}", .{self.angle});
             const t: f64 = @floatFromInt(self.rand.intRangeAtMost(u16, 0, 360));
             self.angle = t * 3.142 / 180;
             self.speed = 0.01;
             self.x = 400;
             self.y = 300;
-            // print("Angle after update: {any}", .{self.angle});
-            //
         }
     }
 };
